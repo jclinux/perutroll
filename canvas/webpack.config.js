e93ee0811d07f0d607a6a84881f5773edc125931
Joseph Esteban <joseph.esteban@fractalservicios.pe>
2017-04-05 11:21:47 -0500

    se finalizo la configuracion de react + webpack 2

---
+++ canvas/webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+ 
+var path = require('path');
+
+module.exports = {
+ entry: path.join(__dirname+'/main.js'), //cual es la entrada de nuestra aplicacion
+ output: {
+   path: path.join(__dirname + '/js'), //donde colocará los archivos al terminar
+   filename: 'app.js' //el nombre de nuestro bundle
+ },
+ devServer: { // opciones para el servidor de desarrollo
+   inline: true, // para que se recargue automáticamente cuando cambie un archivo
+   port: 3333 // puerto donde funcionará el servidor
+ },
+ module: {
+   loaders: [
+     {
+       test: /\.js$/, //probamos por la extensión .js
+       exclude: /node_modules/, //no queremos procesar los archivos en node_modules
+       loader: 'babel-loader', //utilizamos babel
+       query: { //cargamos los presets que instalamos
+         presets: ['es2015', 'react']
+       }
+     }
+   ]
+ }
+};
