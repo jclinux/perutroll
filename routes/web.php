9c1047b83acf2afc005266d2cbe6b3827d5d3512
Joseph Esteban <joseph.esteban@fractalservicios.pe>
2017-04-05 17:15:11 -0500

    ---

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('deploy', 'DeployController@index');
 
 
 Route::get('reactjs',function(){
-	return File::get(public_path() . '/canvas/index.html');
+	return File::get('/canvas/index.html');
 });
